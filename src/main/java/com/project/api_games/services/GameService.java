8f258f3423d219ca02068398715f2ec384daf61e
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-01-30 00:29:02 -0300

    feat: add findById method in GameService class

--- src/main/java/com/project/api_games/services/GameService.java
+++ src/main/java/com/project/api_games/services/GameService.java
@@ -1,12 +1,17 @@
 package com.project.api_games.services;
 
+import com.project.api_games.dtos.GameDTO;
 import com.project.api_games.dtos.GameMinDTO;
 import com.project.api_games.entities.Game;
+import com.project.api_games.exceptions.GameNotFound;
 import com.project.api_games.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,10 +19,19 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
-
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> response = this.gameRepository.findAll();
 
     return response.stream().map(GameMinDTO::new).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public GameDTO findById(UUID id) {
+    Optional<Game> response = this.gameRepository.findById(id);
+
+    if(response.isEmpty()) throw new GameNotFound();
+
+    return new GameDTO(response.get());
   }
 }
