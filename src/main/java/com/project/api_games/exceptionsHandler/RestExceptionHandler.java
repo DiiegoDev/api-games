8eb49de23b990176d8a4c2b1d6d91f134a271df9
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-01-30 00:28:05 -0300

    feat: add RestExceptionHandler class

---
+++ src/main/java/com/project/api_games/exceptionsHandler/RestExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.project.api_games.exceptionsHandler;
+
+import com.project.api_games.exceptions.GameNotFound;
+import com.project.api_games.exceptions.RestErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+  @ExceptionHandler(GameNotFound.class)
+  private ResponseEntity<RestErrorMessage> gameNotFoundHandler(GameNotFound exception) {
+    RestErrorMessage errorResponse = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+  }
+}
