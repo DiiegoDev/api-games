3a1baaac01c79ee38cf793c568ee153f80e834d6
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-01-30 00:29:33 -0300

    feat: add findById method in GameController class

--- src/main/java/com/project/api_games/controllers/GameController.java
+++ src/main/java/com/project/api_games/controllers/GameController.java
@@ -1,14 +1,17 @@
 package com.project.api_games.controllers;
 
+import com.project.api_games.dtos.GameDTO;
 import com.project.api_games.dtos.GameMinDTO;
 import com.project.api_games.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/games")
@@ -21,5 +24,12 @@ public class GameController {
     List<GameMinDTO> response = this.gameService.findAll();
 
     return ResponseEntity.ok().body(response);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<GameDTO> findById(@PathVariable UUID id) {
+    GameDTO response = this.gameService.findById(id);
+
+    return ResponseEntity.ok(response);
   }
 }
