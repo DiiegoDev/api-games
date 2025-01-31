47d2657190bb28ae1e6e4d84fac1c4a37b9fbd7b
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-01-30 23:08:57 -0300

    feat: add searchByList method in GameRepository interface

--- src/main/java/com/project/api_games/repositories/GameRepository.java
+++ src/main/java/com/project/api_games/repositories/GameRepository.java
@@ -1,9 +1,21 @@
 package com.project.api_games.repositories;
 
 import com.project.api_games.entities.Game;
+import com.project.api_games.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GameRepository extends JpaRepository<Game, UUID> {
+  @Query(nativeQuery = true, value = """
+		SELECT game.id, game.title, game.game_year AS `year`, game.img_url AS imgUrl,
+		game.short_description AS shortDescription, belonging.position
+		FROM game
+		INNER JOIN belonging ON game.id = belonging.game_id
+		WHERE belonging.game_list_id = :listId
+		ORDER BY belonging.position
+			""")
+	List<GameMinProjection> searchByList(UUID listId);
 }
