b283a25ba493155a14ae056e313de8aa79203886
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-01-30 23:16:11 -0300

    feat: add findOneList method in GameListController

--- src/main/java/com/project/api_games/controllers/GameListController.java
+++ src/main/java/com/project/api_games/controllers/GameListController.java
@@ -1,17 +1,20 @@
 package com.project.api_games.controllers;
 
 import com.project.api_games.dtos.GameListDTO;
+import com.project.api_games.dtos.GameMinDTO;
 import com.project.api_games.services.GameListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("api/games-list")
+@RequestMapping("api/lists")
 public class GameListController {
   @Autowired
   private GameListService gameListService;
@@ -19,6 +22,13 @@ public class GameListController {
   @GetMapping()
   public ResponseEntity<List<GameListDTO>> findAll() {
     List<GameListDTO> response = this.gameListService.findAll();
+
+    return ResponseEntity.ok().body(response);
+  }
+
+  @GetMapping("/{id}/games")
+  public ResponseEntity<List<GameMinDTO>> findOneList(@PathVariable UUID id) {
+    List<GameMinDTO> response = this.gameListService.findOneList(id);
 
     return ResponseEntity.ok().body(response);
   }
