2f53489527cbf17ab4bf2972eef7046ed5066efb
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-02-04 00:12:24 -0300

    feat: add updateBelongingPosition method in GameListRepository

--- src/main/java/com/project/api_games/repositories/GameListRepository.java
+++ src/main/java/com/project/api_games/repositories/GameListRepository.java
@@ -2,8 +2,16 @@ package com.project.api_games.repositories;
 
 import com.project.api_games.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
-public interface GameListRepository extends JpaRepository<GameList, UUID> {
+public interface GameListRepository extends JpaRepository<GameList, String> {
+  @Modifying
+  @Query(nativeQuery = true, value = """
+      UPDATE belonging SET position = :newPosition 
+      WHERE belonging.game_list_id = :listId AND game_id = :gameId
+  """)
+  void updateBelongingPosition(String listId, String gameId, int newPosition);
 }
